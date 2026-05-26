c54c8b1b534f153510d1f0cd5ae1e0b682aec219
Parv Khandelwal <parv10046@gmail.com>
2026-05-26 18:03:42 +0530

    implement saveLead method to handle WebhookDto and create new Lead entity

--- src/main/java/com/echohype/lead/management/service/LeadService.java
+++ src/main/java/com/echohype/lead/management/service/LeadService.java
@@ -1,11 +1,14 @@
 package com.echohype.lead.management.service;
 
+import com.echohype.lead.management.dto.FieldDto;
 import com.echohype.lead.management.dto.LeadResponseDto;
+import com.echohype.lead.management.dto.WebhookDto;
 import com.echohype.lead.management.entity.Lead;
 import com.echohype.lead.management.entity.Status;
 import com.echohype.lead.management.repository.LeadRepository;
 import com.echohype.lead.management.specification.LeadSpecification;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
@@ -14,6 +17,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class LeadService {
 
     private final LeadRepository leadRepository;
@@ -29,8 +33,49 @@ public class LeadService {
 
     }
 
-    public void saveLead(Lead lead) {
-        leadRepository.save(lead);
+    public void saveLead(WebhookDto dto) {
+        if (dto == null || dto.getFields() == null) {
+            return;
+        }
+
+        Lead newLead = new Lead();
+
+        for (FieldDto field : dto.getFields()) {
+
+
+            if (field.getId() == null || field.getValue() == null || field.getValue().trim().isEmpty()) {
+                continue;
+            }
+
+            String fieldId = field.getId().toLowerCase();
+            String fieldValue = field.getValue().trim();
+
+            switch (fieldId) {
+                case "name":
+                    newLead.setName(fieldValue);
+                    break;
+                case "email":
+                    newLead.setEmail(fieldValue);
+                    break;
+                case "phone":
+                    newLead.setPhoneNumber(fieldValue);
+                    break;
+                case "business_name":
+                    newLead.setBusinessName(fieldValue);
+                    break;
+                case "biggest_challenge":
+                    newLead.setBiggestChallenge(fieldValue);
+                    break;
+            }
+        }
+
+        if (newLead.getName() == null || newLead.getBiggestChallenge() == null) {
+            log.error("Form rejected: Missing mandatory Name or Challenge.");
+            return;
+        }
+
+        leadRepository.save(newLead);
+        log.info("Successfully saved new lead: " + newLead.getName());
     }
 
 
