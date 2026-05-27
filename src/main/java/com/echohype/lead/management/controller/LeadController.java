1b6993f828ac975f8540de8508528e48024b7779
Parv Khandelwal <parv10046@gmail.com>
2026-05-27 14:06:11 +0530

    Add webhook endpoint in LeadController with secret key verification

--- src/main/java/com/echohype/lead/management/controller/LeadController.java
+++ src/main/java/com/echohype/lead/management/controller/LeadController.java
@@ -1,10 +1,12 @@
 package com.echohype.lead.management.controller;
 
 import com.echohype.lead.management.dto.LeadResponseDto;
+import com.echohype.lead.management.dto.WebhookDto;
 import com.echohype.lead.management.entity.Status;
 import com.echohype.lead.management.exception.LeadNotFoundException;
 import com.echohype.lead.management.service.LeadService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +19,23 @@ import java.util.Map;
 public class LeadController {
 
     private final LeadService leadService;
+    @Value("${webhook.secret}")
+    private String secret;
+
+
+    @PostMapping
+    public ResponseEntity<Void>saveLead(
+            @RequestBody WebhookDto dto,
+            @RequestHeader("X-Webhook-Secret") String secretKey){
+
+        if(secret.equals(secretKey)){
+            leadService.saveLead(dto);
+            return ResponseEntity.ok().build();
+        }else{
+            return ResponseEntity.status(403).build();
+        }
+
+    }
 
 
     @GetMapping
