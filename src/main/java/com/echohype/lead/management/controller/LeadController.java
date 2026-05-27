973e760e480bf59ce30707610168809c30491f05
Parv Khandelwal <parv10046@gmail.com>
2026-05-27 13:23:14 +0530

    add updateLead endpoint in LeadController to modify lead status and handle exceptions

--- src/main/java/com/echohype/lead/management/controller/LeadController.java
+++ src/main/java/com/echohype/lead/management/controller/LeadController.java
@@ -2,11 +2,14 @@ package com.echohype.lead.management.controller;
 
 import com.echohype.lead.management.dto.LeadResponseDto;
 import com.echohype.lead.management.entity.Status;
+import com.echohype.lead.management.exception.LeadNotFoundException;
 import com.echohype.lead.management.service.LeadService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/leads")
@@ -33,7 +36,21 @@ public class LeadController {
             }
         }
 
-
-
     }
+    @PatchMapping("/{id}/status")
+    public ResponseEntity<Void>updateLead(
+            @RequestBody Map<String, String> body,
+            @PathVariable Long id){
+
+        try{
+            Status status1 =Status.valueOf(body.get("status").toUpperCase());
+            leadService.updateStatus(id,status1);
+            return ResponseEntity.ok().build();
+        }catch (IllegalArgumentException e) {
+            return  ResponseEntity.badRequest().build();
+        }catch (LeadNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
 }
